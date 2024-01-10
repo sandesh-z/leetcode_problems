e14918ca4484e36ad5a8cd0139d78c80518035c8
sandesh <gsandace@gmail.com>
2024-01-10 21:41:32 +0545

    solution of length of last word

---
+++ lenght_of_last_word/length_of_last_word.py
@@ -0,0 +1,8 @@
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        wordList = s.split()
+        return len(wordList[-1])
+    
+
+s = Solution()
+print(s.lengthOfLastWord("hello world"))
