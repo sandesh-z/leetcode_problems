f760763ea39c23e585a397b87b5807fa47711d60
sandesh <gsandace@gmail.com>
2024-02-11 16:28:23 +0545

    program to count number of complete node in binary tree

---
+++ count_nodes/count_complete_tree_nodes.py
@@ -0,0 +1,46 @@
+"""
+Given the root of a complete binary tree, return the number of the nodes in the tree.
+
+According to Wikipedia, every level, except possibly the last, is completely filled in a complete binary tree, and all nodes in the last level are as far left as possible. It can have between 1 and 2h nodes inclusive at the last level h.
+
+Design an algorithm that runs in less than O(n) time complexity.
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5 * 104].
+0 <= Node.val <= 5 * 104
+The tree is guaranteed to be complete.
+
+"""
+
+# Definition for a binary tree node.
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        count = 0
+        if root:
+            count+=1
+            if root.left:
+                count+1
+            if root.right:
+                count+1
+            return count + self.countNodes(root.left)+self.countNodes(root.right)
+        return count
+    
+s = Solution()
+root = TreeNode(4)
+root.left = TreeNode(2)
+root.right = TreeNode(7)
+root.left.left = TreeNode(1)
+root.left.right = TreeNode(3)
+root.right.left= TreeNode(6)
+root.right.right = TreeNode(9)
+
+print(s.countNodes(root))
