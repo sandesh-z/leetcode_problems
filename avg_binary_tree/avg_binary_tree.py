6ffa50a7ebae1f0ab6092efa49d3839644859510
sandesh <gsandace@gmail.com>
2024-02-15 21:47:10 +0545

    problem description added

--- avg_binary_tree/avg_binary_tree.py
+++ avg_binary_tree/avg_binary_tree.py
@@ -1,11 +1,17 @@
 from typing import List, Optional
 from collections import deque
+
+"""
+Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. 
+Answers within 10-5 of the actual answer will be accepted.
+"""
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
         self.left = left
         self.right = right
 class Solution:
+    #code generated with chatgpt
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         if not root:
             return []
